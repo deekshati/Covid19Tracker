36fbe82459212df64b2179ec98652c8d65cd5e86
deekshati <81002695+deekshati@users.noreply.github.com>
2023-01-03 22:26:41 +0530

    Add files via upload

---
+++ precache-manifest.b1104fd9ffee6da1bb7a20772eb94d09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fe3f42a86a0bfef406a4fd145c2ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e866a55d8ac6505dfc2b",
+    "url": "/static/css/main.a5e6950b.chunk.css"
+  },
+  {
+    "revision": "426c0e0b3174d0f61d01",
+    "url": "/static/js/2.85157e81.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.85157e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e866a55d8ac6505dfc2b",
+    "url": "/static/js/main.1c067008.chunk.js"
+  },
+  {
+    "revision": "7aa1934857ab253cff98",
+    "url": "/static/js/runtime-main.b409363d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
